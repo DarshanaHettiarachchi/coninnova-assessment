f621c99f6683b3bddcb38a4fa2476cab15bc65ca
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-08-14 23:12:22 +0530

    Add efcore dbcontext to infrasturcture project.

---
+++ backend-api/src/CivCost.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CivCost.Domain.PurchaseOrders;
+using CivCost.Domain.Suppliers;
+using Microsoft.EntityFrameworkCore;
+
+namespace CivCost.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public DbSet<Supplier> Suppliers { get; private set; }
+
+    public DbSet<PurchaseOrder> PurchaseOrders { get; private set; }
+
+    public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
