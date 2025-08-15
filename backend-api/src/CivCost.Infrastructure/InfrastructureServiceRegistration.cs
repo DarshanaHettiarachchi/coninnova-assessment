46250e5aa1822108b9b108e3b5685470fda4fa98
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-08-15 23:22:48 +0530

    Add readme file.

---
+++ backend-api/src/CivCost.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CivCost.Infrastructure;
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("CivCostDb")));
+
+        return services;
+    }
+}
