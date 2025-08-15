09f96d931721fe93490afc32075d9f801b9b29dc
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-08-15 23:39:21 +0530

    Add Efcore initial wirings.

---
+++ backend-api/Migrations.md
@@ -0,0 +1,26 @@
+﻿### 🛠 Running EF Core Migrations from Infrastructure Project
+
+This guide shows how to add and apply Entity Framework Core
+migrations when your `DbContext` is located
+inside the `CivCost.Infrastructure` project,
+and your `Program.cs` (startup) is in `CivCost.API`.
+
+---
+
+###  Run following commands
+
+```bash
+dotnet tool install --global dotnet-ef
+
+cd src/CivCost.Infrastructure
+
+
+dotnet ef migrations add [MigratinName] `
+  --project . `
+  --startup-project ..\CivCost.Api `
+  --output-dir Migrations
+
+dotnet ef database update `
+  --project . `
+  --startup-project ..\CivCost.Api
+
--- backend-api/src/CivCost.Api/Program.cs
+++ backend-api/src/CivCost.Api/Program.cs
@@ -1,5 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+using CivCost.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,13 +9,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
