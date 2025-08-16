dd736de8e2fbdc949bb9967f4562b0008b05d463
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-08-16 21:25:04 +0530

    Fix Supplier seed data issue.
    previously new guid was generated for every execution of a migration.

--- backend-api/src/CivCost.Infrastructure/ApplicationDbContext.cs
+++ backend-api/src/CivCost.Infrastructure/ApplicationDbContext.cs
@@ -21,27 +21,27 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
         modelBuilder.Entity<Supplier>().HasData(
-            Supplier.Create(
-               name: "Supplier one",
-               email: "suplierone@email.com",
-               phone: "07712345567"
-            )
-        );
-
-        modelBuilder.Entity<Supplier>().HasData(
-            Supplier.Create(
-               name: "Supplier Two",
-               email: "supliertwo@email.com",
-               phone: "07712345568"
-            )
-        );
-
-        modelBuilder.Entity<Supplier>().HasData(
-            Supplier.Create(
-               name: "Supplier Three",
-               email: "suplierthree@email.com",
-               phone: "07712345569"
-            )
+             new
+             {
+                 Id = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                 Name = "Acme Corp",
+                 Email = "info@acme.com",
+                 Phone = "123-456-7890"
+             },
+              new
+              {
+                  Id = Guid.Parse("11111111-1111-1111-1111-111111111112"),
+                  Name = "Tcme Corp",
+                  Email = "info@Tcme.com",
+                  Phone = "123-466-7890"
+              },
+              new
+              {
+                  Id = Guid.Parse("11111111-1111-1111-1111-111111111113"),
+                  Name = "Rcme Corp",
+                  Email = "info@Rcme.com",
+                  Phone = "123-656-7890"
+              }
         );
 
         base.OnModelCreating(modelBuilder);
