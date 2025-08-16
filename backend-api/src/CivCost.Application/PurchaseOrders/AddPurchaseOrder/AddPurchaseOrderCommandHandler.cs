0d52b0e826653af8ab4166c6b4542f5a77486de1
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-08-16 23:30:20 +0530

    Add Po number generator to the handler.

--- backend-api/src/CivCost.Application/PurchaseOrders/AddPurchaseOrder/AddPurchaseOrderCommandHandler.cs
+++ backend-api/src/CivCost.Application/PurchaseOrders/AddPurchaseOrder/AddPurchaseOrderCommandHandler.cs
@@ -1,25 +1,34 @@
 ﻿using CivCost.Application.Abstractions;
 using CivCost.Domain.Abstractions;
 using CivCost.Domain.PurchaseOrders;
+using CivCost.Infrastructure.Services;
 
 namespace CivCost.Application.PurchaseOrders.AddPurchaseOrder;
 internal sealed class AddPurchaseOrderCommandHandler : ICommandHandler<AddPurchaseOrderCommand>
 {
     private readonly IPurchaseOrderRepository _purchaseOrderRepository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IPurchaseOrderNumberGenerator _purchaseOrderNumberGenerator;
 
-    public AddPurchaseOrderCommandHandler(IPurchaseOrderRepository purchaseOrderRepository, IUnitOfWork unitOfWork)
+    public AddPurchaseOrderCommandHandler(
+        IPurchaseOrderRepository purchaseOrderRepository,
+        IUnitOfWork unitOfWork,
+        IPurchaseOrderNumberGenerator purchaseOrderNumberGenerator
+    )
     {
         _purchaseOrderRepository = purchaseOrderRepository;
         _unitOfWork = unitOfWork;
+        _purchaseOrderNumberGenerator = purchaseOrderNumberGenerator;
     }
 
     public async Task<Result> Handle(AddPurchaseOrderCommand request, CancellationToken cancellationToken)
     {
         var totalAmount = new Money(request.TotalAmount);
 
+        var poNumber = await _purchaseOrderNumberGenerator.GenerateAsync(cancellationToken);
+
         var po = PurchaseOrder.Create(
-           poNumber: "PO-2025-10",
+           poNumber: poNumber,
            description: request.Description,
            orderDate: request.OrderDate,
            totalAmount: totalAmount,
