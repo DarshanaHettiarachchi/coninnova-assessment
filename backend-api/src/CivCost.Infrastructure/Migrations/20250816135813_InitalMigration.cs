8b098bc067c89f8f89afdbe92ef0ebf1fdecc595
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-08-16 20:56:41 +0530

    Add inital migrations.

---
+++ backend-api/src/CivCost.Infrastructure/Migrations/20250816135813_InitalMigration.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CivCost.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Supplier",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Supplier", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PurchaseOrders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PoNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    TotalAmount_Amount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    TotalAmount_Currency = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    SupplierId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PurchaseOrders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PurchaseOrders_Supplier_SupplierId",
+                        column: x => x.SupplierId,
+                        principalTable: "Supplier",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Supplier",
+                columns: new[] { "Id", "Email", "Name", "Phone" },
+                values: new object[,]
+                {
+                    { new Guid("6570bffb-d25b-49af-96ea-d99bb386ae04"), "suplierone@email.com", "Supplier one", "07712345567" },
+                    { new Guid("e1eadaf9-98cc-4146-a4b6-50d92a9f8a51"), "suplierthree@email.com", "Supplier Three", "07712345569" },
+                    { new Guid("fa3093b2-019d-4f04-8d2b-0d606aba46d0"), "supliertwo@email.com", "Supplier Two", "07712345568" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseOrders_SupplierId",
+                table: "PurchaseOrders",
+                column: "SupplierId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PurchaseOrders");
+
+            migrationBuilder.DropTable(
+                name: "Supplier");
+        }
+    }
+}
