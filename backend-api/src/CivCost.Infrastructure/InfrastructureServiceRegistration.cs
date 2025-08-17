8a8fd061744094bcac8b2f3825ceb052c9f0219e
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-08-17 10:25:29 +0530

    Add efcore query logging for development trouble shooting.

--- backend-api/src/CivCost.Infrastructure/InfrastructureServiceRegistration.cs
+++ backend-api/src/CivCost.Infrastructure/InfrastructureServiceRegistration.cs
@@ -7,14 +7,20 @@ using CivCost.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace CivCost.Infrastructure;
 public static class InfrastructureServiceRegistration
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("CivCostDb")));
+        {
+            options.UseSqlServer(configuration.GetConnectionString("CivCostDb"));
+            options.LogTo(Console.WriteLine, LogLevel.Information)
+                   .EnableSensitiveDataLogging(); //This is only for development 
+        });
 
         services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
 
