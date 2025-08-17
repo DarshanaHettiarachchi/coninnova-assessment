125efde0e473d4e6dcd01c868fcd0a80307fc131
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-08-17 14:38:12 +0530

    Add initial ui components for data display.

---
+++ frontend-app/src/app/purchase-orders/ui/data-filters/data-filters.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-filters',
+  imports: [],
+  templateUrl: './data-filters.component.html',
+  styleUrl: './data-filters.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DataFiltersComponent { }
---
+++ frontend-app/src/app/purchase-orders/ui/data-table/data-table.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-table',
+  imports: [],
+  templateUrl: './data-table.component.html',
+  styleUrl: './data-table.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DataTableComponent { }
