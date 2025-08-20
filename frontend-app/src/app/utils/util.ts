0c8a93d3a6d45bf298bb868af64324492367ea5e
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-08-20 11:24:31 +0530

    Add empty string check to the has value util.

--- frontend-app/src/app/utils/util.ts
+++ frontend-app/src/app/utils/util.ts
@@ -1,7 +1,13 @@
 export function hasValue<T>(value: T | null | undefined): value is T {
-  return (
-    value !== null &&
-    value !== undefined &&
-    !(typeof value === 'number' && isNaN(value))
-  );
+  if (value === null || value === undefined) return false;
+
+  if (typeof value === 'number') {
+    return !isNaN(value);
+  }
+
+  if (typeof value === 'string') {
+    return value.trim().length > 0;
+  }
+
+  return true;
 }
